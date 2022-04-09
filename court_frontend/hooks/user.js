30d1b1f6c404779a9177456abfd62e6256a8add5
elite-robz <robshirley67@gmail.com>
2022-04-08 23:48:36 -0600

    fixed login issues with transfer

--- court_frontend/hooks/user.js
+++ court_frontend/hooks/user.js
@@ -1,28 +1,34 @@
 import { fetchJson } from "../lib/api";
+
 import { useQuery, useQueryClient, useMutation } from "react-query";
 
 export const useSignIn = () => {
   const queryClient = useQueryClient();
 
-  const mutation = useMutation(({ email, password }) => {
-    fetchJson("/api/login", {
+  let error = false;
+
+  const handleLog = async (email, password) => {
+    const res = await fetchJson("/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
     });
-  });
+    if (!res) {
+      error = true;
+      throw new Error();
+    }
+  };
 
   return {
     signIn: async (email, password) => {
       try {
-        const user = await mutation.mutateAsync({ email, password });
+        const user = await handleLog(email, password);
         queryClient.setQueriesData("user", user);
         return true;
       } catch (err) {
         return false;
       }
     },
-    signInError: mutation.isError,
-    signInLoading: mutation.isLoading,
+    signInError: error,
   };
 };
