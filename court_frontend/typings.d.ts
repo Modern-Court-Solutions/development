fe5d1a961eeabb983fe569de19bfe558e49c35dc
Rob <rshirley67@gmail.com>
2022-09-25 23:13:21 -0600

    made a mock type for courtcases

---
+++ court_frontend/typings.d.ts
@@ -0,0 +1,16 @@
+type Case = {
+  id: number;
+  name: string;
+  description: string;
+  status: string;
+  created: string;
+  updated: string;
+  creator: string;
+  assignee: string;
+  priority: string;
+  type: string;
+  labels: string[];
+  comments: string[];
+  attachments: string[];
+  judge: string;
+};
