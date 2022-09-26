bb7385afcf30cb1cabd196ae3a1eaee8e6fd567d
Rob <rshirley67@gmail.com>
2022-09-25 18:12:51 -0600

    initialized project, base level route

---
+++ court_frontend/pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Layout from "../components/styles/Layout";
+import "../styles/globals.css";
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Layout token={""}>
+          <Component {...pageProps} />
+        </Layout>
+      </QueryClientProvider>
+    </>
+  );
+}
+
+export default MyApp;
