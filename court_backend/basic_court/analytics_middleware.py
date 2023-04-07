3d1dce0f9fa88be174f92e0d63c10b6cc0e272b1
Jameson Shirley <jameson.shirley24@gmail.com>
2023-04-07 14:28:15 -0600

    changed analytics

--- court_backend/basic_court/analytics_middleware.py
+++ court_backend/basic_court/analytics_middleware.py
@@ -1,35 +1,39 @@
-from django.utils.deprecation import MiddlewareMixin
-import json
-import logging
 from django.utils import timezone
+import logging
 
 logger = logging.getLogger(__name__)
 
-class AnalyticsMiddleware(MiddlewareMixin):
+class AnalyticsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-    def process_request(self, request):
+    def __call__(self, request):
         request.start_time = timezone.now()
 
+        # Store the request body before processing the request
+        request._request_data = request.body.decode('utf-8') if request.body else None
+
+        response = self.get_response(request)
+        self.process_response(request, response)
+        return response
+
     def process_response(self, request, response):
-        if hasattr(request, 'start_time'):
-            duration = timezone.now() - request.start_time
-            user = request.user if request.user.is_authenticated else 'Anonymous'
+        end_time = timezone.now()
+        response_time = (end_time - request.start_time).total_seconds() * 1000
 
-            log_data = {
-                'user': str(user),
-                'method': request.method,
-                'url': request.get_full_path(),
-                'status_code': response.status_code,
-                'duration': duration.microseconds / 1000,
-                'timestamp': request.start_time.strftime("%Y-%m-%d %H:%M:%S"),
-            }
+        log_data = {
+            'user': request.user,
+            'method': request.method,
+            'path': request.path,
+            'request_data': request._request_data,
+            'response_time': response_time,
+        }
 
-            if request.method in ['POST', 'PUT', 'PATCH']:
-                try:
-                    log_data['data'] = json.loads(request.body.decode())
-                except Exception as e:
-                    logger.error(f"AnalyticsMiddleware - Error parsing request body: {e}")
-
-            logger.info(json.dumps(log_data))
+        # Log the data
+        logger.info(log_data)
 
         return response
+
+
+
+
--- court_backend/basic_court/api_views.py
+++ court_backend/basic_court/api_views.py
@@ -17,7 +17,7 @@ from basic_court.models import Case
 class FileCreate(CreateAPIView):
     serializer_class = FileSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def create(self, request, *args, **kwargs):
@@ -28,7 +28,7 @@ class FileRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     lookup_field = 'id'
     serializer_class = FileSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def delete(self, request, *args, **kwargs):
@@ -47,7 +47,7 @@ class AttorneyList(ListAPIView):
     queryset = Attorney.objects.all()
     serializer_class = AttorneySerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('f_name', 'l_name', 'barnum')
@@ -57,7 +57,7 @@ class AttorneyList(ListAPIView):
 class AttorneyCreate(CreateAPIView):
     serializer_class = CreateAttorneySerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def create(self, request, *args, **kwargs):
@@ -68,7 +68,7 @@ class AttorneyRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     lookup_field = 'id'
     serializer_class = CreateAttorneySerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def delete(self, request, *args, **kwargs):
@@ -87,7 +87,7 @@ class ParticipantList(ListAPIView):
     queryset = Participant.objects.all()
     serializer_class = ParticipantSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id', 'f_name', 'l_name')
@@ -97,7 +97,7 @@ class ParticipantList(ListAPIView):
 class ParticipantCreate(CreateAPIView):
     serializer_class = CreateParticipantSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def create(self, request, *args, **kwargs):
@@ -108,7 +108,7 @@ class ParticipantRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     lookup_field = 'id'
     serializer_class = CreateParticipantSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def delete(self, request, *args, **kwargs):
@@ -138,7 +138,7 @@ class CalendarList(ListAPIView):
     queryset = Case.objects.order_by('-date_filed')
     serializer_class = CaseSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
 
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id', 'status', 'title', 'case_type', 'judge', 'location', 'interpreter', 'pro_se_litigant', 'filing_enabled', 'movers', 'responders', 'mover_counsel', 'responder_counsel') 
@@ -147,7 +147,7 @@ class CalendarList(ListAPIView):
 class CaseCreate(CreateAPIView):
     serializer_class = CreateCaseSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     
 
@@ -159,7 +159,7 @@ class CaseRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     lookup_field = 'id'
     serializer_class = CreateCaseSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def delete(self, request, *args, **kwargs):
@@ -178,7 +178,7 @@ class CaseNotesList(ListAPIView):
     queryset = CaseNotes.objects.order_by('-date_created')
     serializer_class = CaseNotesSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id', 'case')
@@ -188,7 +188,7 @@ class CaseNotesList(ListAPIView):
 class CaseNotesCreate(CreateAPIView):
     serializer_class = CaseNotesSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def create(self, request, *args, **kwargs):
@@ -199,7 +199,7 @@ class CaseNotesRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     lookup_field = 'id'
     serializer_class = CaseNotesSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def delete(self, request, *args, **kwargs):
@@ -214,7 +214,7 @@ class JudgeList(ListAPIView):
     queryset = Judge.objects.all()
     serializer_class = JudgeSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id', 'f_name', 'l_name')
@@ -225,7 +225,7 @@ class ChargeList(ListAPIView):
     queryset = Charge.objects.all()
     serializer_class = ChargeSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id', 'code', 'case',)
@@ -235,7 +235,7 @@ class ChargeList(ListAPIView):
 class ChargeCreate(CreateAPIView):
     serializer_class = CreateChargeSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def create(self, request, *args, **kwargs):
@@ -246,7 +246,7 @@ class ChargeRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     lookup_field = 'id'
     serializer_class = CreateChargeSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def delete(self, request, *args, **kwargs):
@@ -261,7 +261,7 @@ class EventList(ListAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id', 'case', 'status', 'event_type',)
@@ -271,7 +271,7 @@ class EventList(ListAPIView):
 class EventCreate(CreateAPIView):
     serializer_class = CreateEventSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def create(self, request, *args, **kwargs):
@@ -282,7 +282,7 @@ class EventRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     lookup_field = 'id'
     serializer_class = CreateEventSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def delete(self, request, *args, **kwargs):
@@ -300,7 +300,7 @@ class CaseSecurityList(ListAPIView):
     queryset = CaseSecurity.objects.all()
     serializer_class = CaseSecuritySerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
@@ -311,7 +311,7 @@ class CaseStatusList(ListAPIView):
     queryset = CaseStatus.objects.all()
     serializer_class = CaseStatusSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
@@ -322,7 +322,7 @@ class CaseTypeList(ListAPIView):
     queryset = CaseType.objects.all()
     serializer_class = CaseTypeSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
@@ -333,7 +333,7 @@ class CaseLocationList(ListAPIView):
     queryset = CaseLocation.objects.all()
     serializer_class = CaseLocationSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
@@ -344,7 +344,7 @@ class EventStatusList(ListAPIView):
     queryset = EventStatus.objects.all()
     serializer_class = EventStatusSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
@@ -355,7 +355,7 @@ class EventTypeList(ListAPIView):
     queryset = EventType.objects.all()
     serializer_class = EventTypeSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
@@ -366,7 +366,7 @@ class DocumentTypeList(ListAPIView):
     queryset = DocumentType.objects.all()
     serializer_class = DocumentTypeSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
@@ -377,7 +377,7 @@ class PaymentTypeList(ListAPIView):
     queryset = PaymentType.objects.all()
     serializer_class = PaymentTypeSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
@@ -389,7 +389,7 @@ class FeeCodeList(ListAPIView):
     queryset = FeeCode.objects.all()
     serializer_class = FeeCodeSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
@@ -399,7 +399,7 @@ class ReportList(ListAPIView):
     queryset = Reports.objects.all()
     serializer_class = ReportSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id', 'name', 'params', 'court', 'user',)
@@ -409,7 +409,7 @@ class ReportList(ListAPIView):
 class ReportCreate(CreateAPIView):
     serializer_class = ReportSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def create(self, request, *args, **kwargs):
@@ -420,7 +420,7 @@ class ReportRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     lookup_field = 'id'
     serializer_class = ReportSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, DjangoModelPermissions,)
+    permission_classes = [GroupPermission]
     
 
     def delete(self, request, *args, **kwargs):
